7fc03143cd2a01b2f331a71c872e0b8d5f9c1d57
Juga Paazmaya <paazmaya@yahoo.com>
2019-04-02 16:47:23 +0300

    Was this how types are thrown in? :santa:

---
+++ types/index.d.ts
@@ -0,0 +1,20 @@
+/**
+ * tozan
+ * https://github.com/paazmaya/tozan
+ * Index filesystem by creating metadata database
+ *
+ * Copyright (c) Juga Paazmaya <paazmaya@yahoo.com> (https://paazmaya.fi)
+ * Licensed under the MIT license
+ */
+
+export = tozan;
+
+declare namespace tozan {
+  export interface TozanOptions {
+    database?: string | false;
+    hash?: string | false;
+    ignoreDotFiles?: boolean | false;
+  }
+}
+
+declare function tozan(directory: string, options?: tozan.TozanOptions): void;
